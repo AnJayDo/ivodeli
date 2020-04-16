dfa7e947934f97141f0d2d1d5d957e0a231d6b4b
Jay An <jayandn1999@gmail.com>
2020-04-17 01:57:50 +0700

    Port 80

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 
 const path = require('path')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 80
 
 const app = express()
 
--- public/burger-nav.js
+++ public/burger-nav.js
@@ -2,7 +2,7 @@ document.getElementById("back-to-top").onclick = function () {
     $('html, body').animate({ scrollTop: 0 }, 1000);
 };
 document.onscroll = function () {
-    if (scrollY > 500) {
+    if (scrollY > 300) {
         document.getElementById("burger-nav").style.display = "inline";
     } else document.getElementById("burger-nav").style.display = "none";
     if(document.getElementsByClassName('navigator-box-list')[0].style.display=="block") {
